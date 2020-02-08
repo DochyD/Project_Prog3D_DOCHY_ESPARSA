b3255b36d587311c266bee33e4839a38074e945e
Dochy <dimitri.dochy59@gmail.com>
2020-02-08 21:47:20 +0100

    J'ai changé trop de trucs ui

---
+++ Project/Project/Assets/Scripts/Weapon/Animation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    Animator anim;
+    void Start()
+    {
+        
+        anim = gameObject.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    public void fireAnimation() {
+        anim.SetBool("canShoot", true);
+        
+        anim.SetBool("canShoot", false);
+    }
+}
